5d3ed29c4f1fd6ccd499ca0fadbf6a8632431d4c
rlsummerscales <rlsummerscales@gmail.com>
2014-05-20 11:06:33 -0400

    download diabetes related abstracts

--- getabstracts.py
+++ getabstracts.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
 # author: Rodney Summerscales
 
-import sys
-import os.path
 import urllib2
-
 import xml.dom
-from xml.dom import minidom
-from xml.dom.minidom import Document
-
 import xmlutil
 
 eutils = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
@@ -28,17 +22,36 @@ eutils = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 #              + '%20AND%20jsubsetaim[text])' \
 #              + '&cmd=DetailsSearch&retmax=500'
 # #             '&term="cardiovascular%20disease AND (Humans[Mesh] AND Randomized Controlled Trial[ptyp] AND jsubsetaim[text])
-searchArgs = '&term=("myocardial%20ischemia"[MeSH%20Terms]'\
+
+# search used for ischemia corpus
+# searchArgs = '&term=("myocardial%20ischemia"[MeSH%20Terms]'\
+#              + '%20AND%20("humans"[MeSH%20Terms]' \
+#              + '%20AND%20Randomized%20Controlled%20Trial[ptyp]'\
+#              + '%20AND%20jsubsetaim[text])' \
+#              + '&cmd=DetailsSearch&retmax=500'
+
+
+# searchArgs = '&term=(("diabetes"[MeSH%20Major%20Topic])' \
+#              + '%20AND%20("cost"[MeSH%20Major%20Topic])' \
+#              + '%20AND%20("humans"[MeSH%20Terms]' \
+#              + '("1996/01/01"[PDat]:"2006/12/31"[PDat])' \
+#              + '%20AND%20Randomized%20Controlled%20Trial[ptyp]' \
+#              + '%20AND%20jsubsetaim[text])' \
+#              + '&cmd=DetailsSearch&retmax=5000'
+
+searchArgs = '&term=(("diabetes")' \
+             + '%20AND%20("cost")' \
              + '%20AND%20("humans"[MeSH%20Terms]' \
-             + '%20AND%20Randomized%20Controlled%20Trial[ptyp]'\
-             + '%20AND%20jsubsetaim[text])' \
-             + '&cmd=DetailsSearch&retmax=500'
-             
+             + '("1996/01/01"[PDat]:"2006/12/31"[PDat])' \
+             + '&cmd=DetailsSearch&retmax=5000'
+
+
 searchCmd = eutils + 'esearch.fcgi?db=pubmed'+ searchArgs
+print searchCmd
 searchResults = urllib2.urlopen(searchCmd)
 
 # parse results
-xmldoc = minidom.parseString(searchResults.read())
+xmldoc = xml.dom.minidom.parseString(searchResults.read())
 idNodeList = xmldoc.getElementsByTagName('Id')
 print 'Number of documents found =', len(idNodeList)
 
@@ -46,16 +59,16 @@ print 'Number of documents found =', len(idNodeList)
 
 # download files
 for idNode in idNodeList:
-  print 'Fetching article with id =', id
-#   id = idNode[:-1]
-#   fetchCmd = 'http://www.ncbi.nlm.nih.gov/pubmed/'+id
-#   doc = urllib2.urlopen(fetchCmd)
-#   out = open(id+'.htm', 'w')
+    print 'Fetching article with id =', id
+    #   id = idNode[:-1]
+    #   fetchCmd = 'http://www.ncbi.nlm.nih.gov/pubmed/'+id
+    #   doc = urllib2.urlopen(fetchCmd)
+    #   out = open(id+'.htm', 'w')
 
-  id = xmlutil.getText(idNode)
-  fetchCmd = eutils+'efetch.fcgi?db=pubmed&id='+id+'&retmode=xml&rettype=abstract'
-  doc = urllib2.urlopen(fetchCmd)
-  out = open(id+'.xml', 'w')
-
-  out.write(doc.read())
-  out.close()
+    id = xmlutil.getText(idNode)
+    fetchCmd = eutils+'efetch.fcgi?db=pubmed&id='+id+'&retmode=xml&rettype=abstract'
+    print fetchCmd
+    doc = urllib2.urlopen(fetchCmd)
+    out = open(id+'.xml', 'w')
+    out.write(doc.read())
+    out.close()
